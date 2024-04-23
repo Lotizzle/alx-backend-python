4a4507b3df15cea16daf94eef9a76a39465c068d
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-04-23 01:50:32 +0000

    Created wait-n async function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,6 +10,15 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    Async coroutine that spawns wait_random
+    n times with the specified max_delay
+
+    paramters:
+    n,
+    max-delay
+    """
+
     coroutines = [wait_random(max_delay) for _ in range(n)]
     results = await asyncio.gather(*coroutines)
     return results
