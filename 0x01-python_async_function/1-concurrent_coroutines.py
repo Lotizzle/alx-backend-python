f11ff60c54de0ad7824380ccc6d8c37609743a08
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-04-23 02:34:34 +0000

    Minor fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -21,4 +21,4 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
 
     coroutines = [wait_random(max_delay) for _ in range(n)]
     results = await asyncio.gather(*coroutines)
-    return results
+    return sorted(results)
