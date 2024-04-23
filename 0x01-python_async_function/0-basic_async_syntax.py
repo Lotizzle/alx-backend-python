35aa376166177ae133a98d158c0f9cb2ca38fdff
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-04-23 01:47:29 +0000

    Created wait-n async function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,6 +9,13 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """
+    Async coroutine that waits for a
+    random delay between 0 and max_delay
+
+    parameter:
+    max_delay
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
