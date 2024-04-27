a9718bb0dd3b51b70b157d7d3e99b669b8fb54fb
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-04-27 22:25:27 +0000

    Created type annoted function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# This module contains a type-annotated function, add.
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of a and b as float
+    Parameters: a, b
+    """
+
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,2 @@
+## 0x00. Python - Variable Annotations
+### Learning about using typing in python
