a09df68e62e0ef4f1b1e55ddfdb8b875ec941390
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-04-27 23:39:44 +0000

    Floor it!

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module contains a type-annotated function floor
+that returns the floor of a float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    This method floors a float
+    parameter:n
+    """
+
+    return math.floor(n)
