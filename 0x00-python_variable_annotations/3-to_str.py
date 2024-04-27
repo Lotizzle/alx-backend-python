da8b5313b54ef03ec83c37ff7090ecfaadc84e8d
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-04-27 23:46:17 +0000

    Stringify

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module contains a type-annotated function to_str
+that returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This method takes a float and returns the string representation
+    parameter: n(float)
+    """
+
+    return str(n)
