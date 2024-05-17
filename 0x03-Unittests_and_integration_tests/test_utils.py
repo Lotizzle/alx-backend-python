3309f8f6f3eb2e43e20e6868c0a718d9eac0a981
“Lotizzle <lotharmbaegbu@gmail.com>
2024-05-17 17:00:28 +0000

    Tested utils.get_json function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,8 @@ This module contains the test case for the utils module
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from unittest.mock import Mock, patch
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -44,6 +45,31 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
         self.assertEqual(str(context.exception), expected_error_message)
+
+class TestGetJson(unittest.TestCase):
+    """ Class to test utils.get_json method """
+
+    @patch('utils.requests.get')
+    def test_get_json(self, mock_get):
+        """
+        Test that utils.get_json returns the expected result.
+        """
+        test_cases = [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False})
+        ]
+
+        for test_url, test_payload in test_cases:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            result = get_json(test_url)
+
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
+
+            mock_get.reset_mock()
 
 
 if __name__ == '__main__':
