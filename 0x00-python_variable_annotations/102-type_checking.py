37afccc1f097bc6abb3d9fee658c6ea829a0bbc1
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-07-07 21:13:52 +0100

    Fix

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,24 +4,24 @@ This module contains zoom array function
 """
 
 
-from typing import Tuple, Any
+from typing import List, Tuple
 
 
-def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> Tuple[Any, ...]:
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
     """
-    This method zooms in on an array
-    parameters: lst, factor
+    Zoom array function
     """
-
-    zoomed_in: Tuple[Any, ...] = tuple(
+    zoomed_in: List[int] = [
         item for item in lst
-        for i in range(factor)
-    )
+        for _ in range(factor)
+    ]
     return zoomed_in
 
 
 array = (12, 72, 91)
 
+
 zoom_2x = zoom_array(array)
+
 
 zoom_3x = zoom_array(array, 3)
