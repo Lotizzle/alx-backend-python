0fe90d7ec919d0a55bd4b5154188906ae16ce94d
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-07-07 21:01:47 +0100

    Pycode

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -11,7 +11,11 @@ from typing import TypeVar, Mapping, Any, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(
+    dct: Mapping,
+    key: Any,
+    default: Union[T, None] = None
+) -> Union[Any, T]:
     """
     This method returns the value of a dict using a key
     parameters: dct, key, default
